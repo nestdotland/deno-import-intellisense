3ddf0faec2a4fb98a1be3783144a8bc0fcfddebf
Maximous Black <maximousblk@gmail.com>
2021-04-14 13:08:21 +0530

    fix: unhandled promise rejection

--- api/modules/[module]/versions/index.ts
+++ api/modules/[module]/versions/index.ts
@@ -4,7 +4,7 @@ import fetch from 'node-fetch';
 export default async (req: VercelRequest, res: VercelResponse) => {
   const { module: moduleName } = req.query;
   const moduleInfo = await fetch('https://x.nest.land/api/package/' + moduleName).then((data) => data.json());
-  const moduleVersionList = moduleInfo.moduleUploadNames.map((version: string) =>
+  const moduleVersionList = moduleInfo.packageUploadNames.map((version: string) =>
     version.replace(moduleName + '@', '')
   );
   res.status(200).json(moduleVersionList.reverse());
