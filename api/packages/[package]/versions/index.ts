5631c19eeca4af6bee7c7bfef4640629d3cbaa49
Maximous Black <maximousblk@gmail.com>
2021-04-14 09:33:59 +0530

    fix: type errors

--- api/packages/[package]/versions/index.ts
+++ api/packages/[package]/versions/index.ts
@@ -4,6 +4,6 @@ import fetch from 'node-fetch';
 export default async (req: VercelRequest, res: VercelResponse) => {
   const packageName = req.query.package;
   const packageInfo = await fetch('https://x.nest.land/api/package/' + packageName).then((res) => res.json());
-  const packageVersions = packageInfo.packageUploadNames.map((version) => version.replace(packageName + '@', ''));
+  const packageVersions = packageInfo.packageUploadNames.map((version:string) => version.replace(packageName + '@', ''));
   res.status(200).json(packageVersions.reverse());
 };
--- api/packages/index.ts
+++ api/packages/index.ts
@@ -1,8 +1,8 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
+import { VercelResponse } from '@vercel/node';
 import fetch from 'node-fetch';
 
-export default async (req: VercelRequest, res: VercelResponse) => {
+export default async (res: VercelResponse) => {
   const packagesInfo = await fetch('https://x.nest.land/api/packages').then((res) => res.json());
-  const packageList = packagesInfo.map(({ name }) => name);
+  const packageList = packagesInfo.map(({ name }: {name:string}) => name);
   res.status(200).json(packageList);
 };
