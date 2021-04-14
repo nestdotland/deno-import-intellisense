566a27ea99937c7c92876ea657dc362c105affd5
Maximous Black <maximousblk@gmail.com>
2021-04-14 13:21:55 +0530

    BREAKING: refactor url paths

--- api/x/[module]/[version]/index.ts
+++ api/x/[module]/[version]/index.ts
@@ -6,6 +6,6 @@ export default async (req: VercelRequest, res: VercelResponse) => {
   const moduleInfo = await fetch('https://x.nest.land/api/package/' + moduleName + '/' + moduleVersion).then((data) =>
     data.json()
   );
-  const modulePaths = Object.keys(moduleInfo.files).map((key) => key);
+  const modulePaths = Object.keys(moduleInfo.files).map((key) => key.replace("/", ""));
   res.status(200).json(modulePaths);
 };
